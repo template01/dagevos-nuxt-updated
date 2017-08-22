634319d0384c62427d9e3fa63f6b1a4b37110a76
Lasse <contact@template01.info>
2017-08-22 14:19:37 +0200

    ammended the scroll for hash anchors

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,34 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'starter',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#552E87' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#552E87'
+  },
   css: [
     // { src: '~assets/css/main1.scss', lang: 'scss' },
     '~assets/css/main.css',
@@ -28,7 +40,51 @@ module.exports = {
 
   ],
 
-  plugins: ['~plugins/bootstrap-vue-plugin','~/plugins/lodash-plugin.js','~/plugins/vue-resource-plugin.js',{ src: '~/plugins/vueawesomeswiper-plugin.js', ssr: false }],
+  plugins: ['~plugins/bootstrap-vue-plugin', '~/plugins/lodash-plugin.js', '~/plugins/vue-resource-plugin.js', {
+    src: '~/plugins/vueawesomeswiper-plugin.js',
+    ssr: false
+  }],
+
+
+  router: {
+    scrollBehavior: function(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+        alert(savedPosition)
+
+      } else {
+        let position = {}
+        // if no children detected
+        if (to.matched.length < 2) {
+          // scroll to the top of the page
+          position = {
+            x: 0,
+            y: 0
+          }
+        } else if (to.matched.some((r) => r.components.default.options.scrollToTop)) {
+          // if one of the children has scrollToTop option set to true
+          position = {
+            x: 0,
+            y: 0
+          }
+        }
+        // if link has anchor,  scroll to anchor by returning the selector
+        if (to.hash) {
+          setTimeout(function() {
+            window.location.href = to.hash;
+          }, 100)
+          setTimeout(function() {
+            window.location.href = to.hash;
+          }, 500)
+          setTimeout(function() {
+            window.location.href = to.hash;
+          }, 1400)
+        }else{
+          return position
+        }
+      }
+    }
+  },
 
 
   /*
@@ -39,6 +95,6 @@ module.exports = {
      ** Run ESLINT on save
      */
 
-    vendor: ['axios','bootstrap-vue','vue-awesome-swiper']
+    vendor: ['axios', 'bootstrap-vue', 'vue-awesome-swiper']
   }
 }
